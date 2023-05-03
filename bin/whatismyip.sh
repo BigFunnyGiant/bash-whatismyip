d5c0cc5bce63c40893a268329d395902f63dd9ad
BigFunnyGiant <giantfromjax@gmail.com>
2023-05-03 18:16:39 -0400

    Update whatismyip.sh

--- bin/whatismyip.sh
+++ bin/whatismyip.sh
@@ -1,48 +1,46 @@
 #!/usr/bin/env bash
 
-# Get IP version option from command line arguments or user input
-if [ "$#" -gt 0 ]; then
+# Define services for fetching IPV4 and IPV6 addresses
+_wimip_svc_ipv4="http://ipv4.whatismyip.akamai.com
+                 http://ipv4.tyk.nu
+                 http://ipv4.icanhazip.com
+                 http://ipv4.ifconfig.me"
+
+_wimip_svc_ipv6="http://ipv6.whatismyip.akamai.com
+                 http://ipv6.tyk.nu
+                 http://ipv6.icanhazip.com
+                 http://ipv6.ifconfig.me"
+
+# Check if IP version was provided as command line argument
+if [[ $# -eq 0 ]]; then
+    # Prompt the user to select IPV4 or IPV6
+    read -p "Enter 4 for IPV4 or 6 for IPV6: " ip_version
+else
     ip_version=$1
+fi
+
+# Select the appropriate service based on the user's choice
+if [[ "$ip_version" == "4" ]]; then
+  _wimip_svcs=$_wimip_svc_ipv4
+elif [[ "$ip_version" == "6" ]]; then
+  _wimip_svcs=$_wimip_svc_ipv6
 else
-    read -p "Enter IP version to use (4/6/B for both): " ip_version
+  echo "Invalid selection. Please enter 4 or 6."
+  exit 1
 fi
 
-# Check if IP version is valid
-if [ "$ip_version" != "4" ] && [ "$ip_version" != "6" ] && [ "$ip_version" != "B" ] && [ "$ip_version" != "b" ]; then
-    echo "Invalid option. Please enter 4, 6, or B for both."
-    exit 1
+# Get a random line out of # of lines in _wimip_svcs
+_wimip_rand=$[ $RANDOM % `echo "$_wimip_svcs" | wc -l` ]
+if (( _wimip_rand == 0 )); then
+    _wimip_rand=$[ $RANDOM % `echo "$_wimip_svcs" | wc -l` ]
 fi
-
-# URLs for getting IP addresses
-ipv4_urls="http://ipv4.whatismyip.akamai.com
-           http://ipv4.tyk.nu
-           http://ipv4.icanhazip.com
-           http://ipv4.ifconfig.me"
-
-ipv6_urls="http://ipv6.whatismyip.akamai.com
-           http://ipv6.tyk.nu
-           http://ipv6.icanhazip.com
-           http://ipv6.ifconfig.me"
-
-# Get a random URL from the list for the specified IP version
-if [ "$ip_version" = "4" ]; then
-    urls=$ipv4_urls
-elif [ "$ip_version" = "6" ]; then
-    urls=$ipv6_urls
+# Grab that line and cleanup spaces & tabs
+_wimip_svc=`echo "$_wimip_svcs" | sed -n "$_wimip_rand"p | sed -e 's#[\t ]\+##g'`
+# Hit the service to get IP
+_wimip_myip=`curl -s $_wimip_svc` || echo "Failed: $_wimip_svc"
+# Echo IP out
+if [[ `echo "$_wimip_myip" | grep "[\<\>\/]"` != "" ]]; then
+   echo "Failed: $_wimip_svc"
 else
-    urls="$ipv4_urls"$'\n'"$ipv6_urls"
+   echo -ne "$_wimip_myip\n"
 fi
-
-url=$(echo "$urls" | shuf -n 1)
-
-# Get IP address from the selected URL
-ip=$(curl -s "$url")
-
-# Check if the IP address was retrieved successfully
-if [ -z "$ip" ]; then
-    echo "Failed to retrieve IP address from $url"
-    exit 1
-fi
-
-# Print the IP address
-echo "$ip"
